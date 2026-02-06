96212f4f8d7c22c71fdec2150c16f17fcd1675da
Bhakti_QA <helloooworld650@gmail.com>
2026-02-06 19:05:43 +0530

    MaxChrome

---
+++ demolaunch.spec.js
@@ -0,0 +1,21 @@
+const { chromium } = require("playwright");
+
+( async() => {
+    const browser = await chromium.launch({
+        headless: false, args:['--start-maximized']
+    });
+
+});
+
+const {test, expect} = require ('@playwright/test');
+
+test("Launch Chromium and open the webpage of the Playwright", async({page}) =>
+{
+    await page.waitForLoadState('networkidle');
+    await page.goto('https://demoqa.com/automation-practice-form');
+    await expect(page).toHaveURL("https://demoqa.com/automation-practice-form");
+    await page.getByRole('heading', {name: 'Practice Form'}).click();
+    console.log('Practice.. Heading Verified');
+    await expect(page.getByRole('heading', {name: 'Student Registration Form'})).toBeVisible();
+    console.log('Student.. Heading Verified');
+});
